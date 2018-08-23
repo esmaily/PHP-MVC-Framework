1cdc71f4bd9c69da0d982434a629158258098f6d
esmaily <jaffar9898@gmail.com>
2018-08-24 00:38:11 +0430

    refactoring codes;

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
-
+/*
+ * 18th National Skills Competition  trade of Web Design And Development
+ * Competitor : Seyed Jaffar Esmaili
+ * GitHub : esmaily
+ * Phone : 09145985243
+ *
+ * */
 use App\Core\Bootstrap;
 include '../vendor/autoload.php';
 //set_error_handler('handler');
