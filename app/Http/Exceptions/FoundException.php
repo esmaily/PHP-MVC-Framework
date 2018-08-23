02b1d3475f26caf9f2d912fff43002f434790d90
esmaily <jaffar9898@gmail.com>
2018-08-23 14:46:49 +0430

    add run method for dispatch custom execption and optimize

--- app/Http/Exceptions/FoundException.php
+++ app/Http/Exceptions/FoundException.php
@@ -7,14 +7,11 @@ namespace App\Http\Exceptions;
 class FoundException extends \Exception
 {
 	private static $_typeExceptions;
-	private $_exceptions = [];
-	public function __construct ($title = "",$message='')
+	private $_exceptions;
+
+	public function run ($title=NULL, $message=NULL,$file=NULL,$line=NULL)
 	{
-		print_r($this->templateException($title,$message));
-	}
-	public function run ($title, $message)
-	{
-		print_r($this->templateException($title, $message));
+		print_r($this->templateException($title, $message,$file,$line));
 	}
 
 	public static function init ()
@@ -25,14 +22,13 @@ class FoundException extends \Exception
 			4 => 'Parse Exception ',
 			8 => 'Notice',
 		];
-		ini_set('display_errors', 0);
+//		ini_set('display_errors', 0);
 		error_reporting(E_ALL);
 		register_shutdown_function('handler');
 	}
 
 	public function handler ()
 	{
-
 		if (is_null($exceptionDetail = error_get_last()) === FALSE) {
 			foreach ($exceptionDetail as $key => $value) {
 				$this->_exceptions[$key] = $value;
@@ -41,10 +37,12 @@ class FoundException extends \Exception
 		}
 	}
 
-	private function templateException ($title = NULL, $exception = NULL)
+	private function templateException ($title = NULL, $exception = NULL,$file=NULL,$line=NULL)
 	{
 
 		$title     = $title == NULL ? self::$_typeExceptions[$this->_exceptions['type']] : $title;
+		$file     = $file == NULL ? $this->_exceptions['file']: $file;
+		$line     = $line == NULL ? $this->_exceptions['line'] : $line;
 		$exception = $exception == NULL ? $this->_exceptions['message'] : $exception;
 		$type      = $this->customColor($this->_exceptions['type']);
 		$message   = "<body style='margin: 0;padding: 0'><div style='text-align: center;margin: 0 auto;background: #eee;padding:5% 10%;height: 100%;'>";
@@ -54,9 +52,9 @@ class FoundException extends \Exception
 		$message   .= "<span style='font-size: 20px;color:{$type};text-transform: capitalize;'>{$title}";
 		$message   .= "</span></h3></div>";
 		$message   .= "<div class='body' style='padding:10px 4%;background: #fff;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px'>";
-		$message   .= "<p style='font-size: 18px;color: #333;'><span style='text-transform: capitalize;font-weight: bolder'>File : </span>{$this->_exceptions['file']}</p>";
-		$message   .= "<p style='font-size: 18px;color: #333;'><span style='text-transform: capitalize;font-weight: bolder'>In Line : </span>{$this->_exceptions['line']}</p>";
-		$message   .= "<p style='text-transform: capitalize;font-size: 18px;color: #333;'>{$exception}</p>";
+		$message   .= "<p style='font-size: 18px;color: #333;'><span style='text-transform: capitalize;font-weight: bolder'>File : </span>{$file}</p>";
+		$message   .= "<p style='font-size: 18px;color: #333;'><span style='text-transform: capitalize;font-weight: bolder'>In Line : </span>{$line}</p>";
+		$message   .= "<p style='text-transform: capitalize;font-size: 18px;color: #333;'><span style='text-transform: capitalize;font-weight: bolder'>Message : </span>{$exception}</p>";
 		$message   .= "</div>";
 		$message   .= "</div></div></body>";
 
