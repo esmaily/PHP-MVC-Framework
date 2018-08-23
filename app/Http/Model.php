e9f1ea467d5171e2349aa02c80afac3d8667a9ce
esmaily <jaffar9898@gmail.com>
2018-08-23 20:02:56 +0430

    optimize and change strutcher

--- app/Http/Model.php
+++ app/Http/Model.php
@@ -13,15 +13,12 @@ class Model
 	protected $_fields = [];
 	protected $_primaryKey = 'id';
 	private static $_fetchMode = \PDO::FETCH_ASSOC;
-	protected static $_tableName;
 
 	public function __construct ()
 	{
-
-		$this->_fields = array_values($this->_fields);
+		$this->_setFields();
 		if ($this->_table === NULL) {
-			$this->_table       = Enviroment::get('database')['table_prefix'] . strtolower(str_replace('App\\', '', get_class($this))) . 's';
-			static::$_tableName = $this->_table;
+			$this->_table = Enviroment::get('database')['table_prefix'] . $this->tableOfModel();
 		}
 		$this->_initDatabase();
 	}
@@ -37,6 +34,16 @@ class Model
 			echo "Database Error " . $Error->getMessage();
 		}
 	}
+
+	private function _setFields ()
+	{
+		foreach ($this->_fields as $key => $field) {
+			unset($this->_fields[$key]);
+			$this->_fields[$field] = NULL;
+		}
+
+	}
+
 	# Get Table name as Class
 	public static function table ($tableName)
 	{
@@ -58,7 +65,7 @@ class Model
 	# Select as Database width differ Arguments
 	public function select ($arguments)
 	{
-		$fields = [];;
+		$fields = [];
 		foreach ($arguments as $prop => $conf) {
 			if (is_array($conf)) {
 				$comb             = ' ' . (isset($conf[2]) ? strtoupper($conf[2]) : 'AND') . ' ';
@@ -76,28 +83,36 @@ class Model
 		$stmt   = $this->getDatabase()->prepare($query);
 		$stmt->execute($arguments);
 		$result = $stmt->fetchAll();
-
+		d($query);
 		return $this->_convertRowToObject($result);
 	}
 
 	# Get First Column
-	public function first ($key)
+	public static function first ($key)
 	{
-		$pk    = $this->_primaryKey;
-		$query = "SELECT * FROM  `{$this->_table}` WHERE `{$pk}` = :{$pk}";
-		$stmt  = $this->getDatabase()->prepare($query);
+		$class  = get_called_class();
+		$object = new $class;
+		$table  = $object->getTable();
+		$pk     = $object->_primaryKey;
+		$query  = "SELECT * FROM  `{$table}` WHERE `{$pk}` = :{$pk}";
+		$stmt   = $object->getDatabase()->prepare($query);
 		$stmt->execute([$pk => $key]);
 		$result = $stmt->fetch(self::$_fetchMode);
-		return $this->_convertRowToObject($result);
+
+		return $object->_convertRowToObject($result);
 	}
 
-	public function find ($key)
+	public static function find ($key)
 	{
-		$pk    = $this->_primaryKey;
-		$query = "SELECT * FROM  `{$this->_table}` WHERE `{$pk}` = :{$pk}";
-		$stmt  = $this->getDatabase()->prepare($query);
+		$class  = get_called_class();
+		$object = new $class;
+		$table  = $object->getTable();
+		$pk     = $object->_primaryKey;
+		$query  = "SELECT * FROM  `{$table}` WHERE `{$pk}` = :{$pk}";
+		$stmt   = $object->getDatabase()->prepare($query);
 		$stmt->execute([$pk => $key]);
 		$result = $stmt->fetch(self::$_fetchMode);
+
 		return $result;
 
 	}
@@ -121,11 +136,12 @@ class Model
 	# Select as Database By Param
 	public function findBy ($value, $params)
 	{
+
 		$query = "SELECT *  FROM  `{$this->_table}` WHERE `{$params}` = :{$params}";
 
 		$stmt = $this->getDatabase()->prepare($query);
 		$stmt->execute([$params => $value]);
-		$result = $stmt->fetchAll();
+		$result = $stmt->fetchAll(self::$_fetchMode);
 
 		return $this->_convertRowToObject($result);
 	}
@@ -133,7 +149,9 @@ class Model
 	# Get All Records Table
 	public static function all ($fields = NULL)
 	{
-		$table = self::$_tableName;
+		$class  = get_called_class();
+		$object = new $class;
+		$table  = $object->getTable();
 		if ($fields == NULL) {
 			$sql = "SELECT * FROM `{$table}` ORDER BY `id` DESC ";
 		} elseif (is_string($fields)) {
@@ -163,14 +181,13 @@ class Model
 
 	public function update ()
 	{
-		$this->getFields();
-		$pkKey = $this->_primaryKey;
-//		$this->_checkFields();
-		$keys   = array_keys($this->_fields);
-//		$fields = implode(',', wrapValue($this->_fields, '`', 'var'));
-//		$query  = "UPDATE `{$this->_table}` SET  {$fields} WHERE `{$pkKey}` =:{$pkKey}";
-		d($keys);
-		dump('d');
+
+		$fields = $this->_fields;
+		$pkKey  = $this->_primaryKey;
+		unset($fields[$pkKey]);
+		$fields = implode(',', wrapValue(array_keys($fields), '`', 'var'));
+		$query  = "UPDATE `{$this->_table}` SET  {$fields} WHERE `{$pkKey}` =:{$pkKey}";
+
 		return $this->getDatabase()->prepare($query)->execute($this->_fields);
 	}
 
@@ -198,31 +215,47 @@ class Model
 		return count($errors) === 0 ? TRUE : FALSE;
 	}
 
-	# Row Into Object
+	# Row Change Into Object
 	private function _convertRowToObject ($resource)
 	{
 		$objects = [];
-		foreach ($resource as $in=>$row) {
-			$className = get_class($this);
-			$object    = new $className;
-			$object->$in = $row ;
-			$objects[] = $object;
-		}
-		if (count($objects) == 1) {
-			return $objects[0];
-		}
-		return $object;
+		$check   = count($resource) == count($this->_fields) ? TRUE : FALSE;
+		if (count($resource) > 0):
+			foreach ($resource as $in => $row) {
+				$className = get_class($this);
+				$object    = new $className;
+				$fields    = array_keys($object->_fields);
+				foreach ($fields as $fieldName) {
+					$check ? $object->$fieldName = $resource[$fieldName] : $object->$fieldName = $row[$fieldName];
+				}
 
+				$objects[] = $object;
+			}
+
+			return $check ? $objects[0] : $objects;
+		endif;
+
+		return $resource;
+
+	}
+
+	private function tableOfModel ()
+	{
+		$class = get_called_class();
+		$class = str_replace('App\\', '', $class);
+		$class = strtolower($class) . 's';
+
+		return $class;
 	}
 
 	# Setter
 	public function __set ($name, $value)
 	{
 		$class = get_class($this);
-		if (in_array($name, $this->_fields)) {
+		if (in_array($name, array_keys($this->_fields))) {
 			return $this->_fields[$name] = $value;
 		}
-		(new FoundException)->run('Property Error ', "The Property (<b >{$name}</b>) dose not exists in  <b>{$class}</b> class  ");
+		(new FoundException)->run('Property Error ', "The Property (<b >{$name}</b>) dose not exists in  <b>{$class}</b> class s ");
 	}
 
 	# Getter
@@ -248,11 +281,10 @@ class Model
 			}
 		} elseif (substr($funcName, 0, strlen('find')) === 'find') {
 			if (!is_int($args[0])) {
-				echo 'is array ot';
 				if (isset($args[0])) {
 					foreach ($args[0] as $prop => $value) {
 						if (!in_array($prop, array_keys($this->getFields()))) {
-							throw new \Exception("Property {$prop} No Is This class");
+							(new FoundException())->run("Property Error", "Property {$prop} No Is This class");
 						}
 
 						return call_user_func_array([$this, 'select'], $args);
@@ -261,8 +293,6 @@ class Model
 			} elseif (is_int($args[0])) {
 				//				echo 'is int';
 				return call_user_func_array([$this, 'first'], $args);
-			} else {
-				echo 'no user';
 			}
 
 
@@ -273,20 +303,19 @@ class Model
 	# Return Fields
 	public function getFields ()
 	{
-		$fields=[];
-		foreach ($this->_fields as $key => $val) {
-			if (is_int($key)) {
-				unset($this->_fields[$key]);
-			}
-			$fields[$key]=$val;
-		}
-		return $fields;
+		return $this->_fields;
 	}
 
 	# Return Database
 	public function getDatabase ()
 	{
 		return $this->_database;
+	}
+
+	# Return Table name
+	public function getTable ()
+	{
+		return $this->_table;
 	}
 
 }
